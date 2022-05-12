fac3b715d03a02417f572df11f1c33a0df3de931
vivek mehra <vivek.mehra@myzeller.com>
2022-05-12 19:42:57 +0530

    removed aws keys

--- vd-server/utils/dynamoDbUtils.js
+++ vd-server/utils/dynamoDbUtils.js
@@ -1,11 +1,11 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  region: 'ap-southeast-2',
-  accessKeyId: 'ASIA3QJSN5CWXM2L3XN4',
-  secretAccessKey: 'QzFgPfAqIShcfZ/4Fj6N4DkSPKxyoBfJO53M0W/t',
+  region: ''
+  accessKeyId: '',
+  secretAccessKey: '',
   sessionToken:
-    'IQoJb3JpZ2luX2VjEFUaDmFwLXNvdXRoZWFzdC0yIkgwRgIhAJDZovc82Eh48dHNn1IXWqqGlcXYxMLHvcPBIrEfTg9nAiEAhj1pr8FAkS6ulipEUNfr7HvsSP6PlkY7YpE2Qdcy+aEqmgMILhACGgw3OTA5MTY2MjI1MDkiDID0TA2u8PEhJQD4XCr3As8T7I/9kkWNevZ4dQKMI5EaRLUQbC/RTo84jq47gUaiH1onVBbc1st1O+dy7JCvC8nelVcd/Vg/MzJ+xYqeDkjJd3sU64IH4hxRWtVR8aytitf5wh3hV2ygTcp0UgrlvzY7FSle3YWLu7RWWSCTEUNy+/gnY4xm8tIUKxUD/9Ip7bHRm8pHwCdD+xQeEJNFGYmvCoil4kY8UgHYssdvigElDfwEjZ7AMlZ67dnvfChueIoyUh+AdkppxbpsVovjuRpF4yMjQ9gzI0ae2yB1/kFXeXz0ghHBWSdmdgUYpCM9bPC3wV/4O29fhVQPT0I0d6YgiZOSYWcQy0lSynk2FtNUAYHxyQKCVehaX1pNxOh2uyVkTnq7RH2m6D1N/Z5iU6Iyf2GDijOL3mvUPPpiUzfaMuFQbtGtfd09WlCAjqXPn3TJtzhD7wYkx/WPLtJbwu/JzBy1YsbDxn+5f26WZw7uUVlEPDUeiwgc98IFSYKfLaK/Fzhh0TCj5O6TBjqlAVDyOQRrNVRxb90OFAU+hzr5WJtiO9+DAuxlzfahqjfqS+ifopHItIQem/JFDzqr8x6w6xqUJ6yhna3pd2WDOB7FD1ye83Ua1tig0WlbN08sLPTvN2kaNWkkYp2jlKryJu2tQD4MzSncXT8JgXqH6yKyngbvORK4llSxKsyuTNBck9DhJBk//MZ8oX7NUDvVNtcnHm0x1U7/h5KN5J8XbpSI4UM2cg==',
+    ''
 });
 
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
